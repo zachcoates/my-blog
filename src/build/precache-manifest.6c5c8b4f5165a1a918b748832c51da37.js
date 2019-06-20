c5454dd8fee3d2baa5a1a899e2f1edf4b1bdeba9
zach Coates <frunkducksapps@gmail,com>
2019-06-20 17:42:36 -0300

    Initial commit of blog site with upvoting and comments functionality

---
+++ src/build/precache-manifest.6c5c8b4f5165a1a918b748832c51da37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da4e614d21bec5f78fd218ff35b668c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c91b7dffecf10504a573",
+    "url": "/static/css/main.c58b48e1.chunk.css"
+  },
+  {
+    "revision": "336c275b236f581a2af8",
+    "url": "/static/js/2.7222f484.chunk.js"
+  },
+  {
+    "revision": "c91b7dffecf10504a573",
+    "url": "/static/js/main.e3d0b22a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
